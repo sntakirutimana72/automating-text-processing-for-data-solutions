53477bf1dfc4d94ccf4b2c2acc681908708317e8
Steve <sntakirutimana72@gmail.com>
2025-05-01 13:08:39 +0200

    [Setup] Initialize JFX project boilerplate

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.automating_text_processing {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.automating_text_processing to javafx.fxml;
+    exports com.automating_text_processing;
+}
