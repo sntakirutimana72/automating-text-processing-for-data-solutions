2f1c338224ae15c02cb5109ad12f948b4ae2d15e
Steve <sntakirutimana72@gmail.com>
2025-05-04 11:11:29 +0200

    [Configuration] Added lombok to project dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,21 @@
 module com.automating_text_processing {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
+  requires org.controlsfx.controls;
+  requires com.dlsc.formsfx;
+  requires net.synedra.validatorfx;
+  requires org.kordamp.ikonli.javafx;
+  requires org.kordamp.bootstrapfx.core;
+  requires static lombok;
 
-    opens com.automating_text_processing to javafx.fxml;
-    exports com.automating_text_processing;
+  opens com.automating_text_processing to javafx.fxml;
+  exports com.automating_text_processing;
+  opens com.automating_text_processing.controller.editor to javafx.fxml;
+  exports com.automating_text_processing.controller.editor;
+  exports com.automating_text_processing.service;
+  opens com.automating_text_processing.service to javafx.fxml;
+  exports com.automating_text_processing.rule;
+  exports com.automating_text_processing.function;
 }
