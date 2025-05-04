4106d6676ddb6693b5d3c9f3eebe163e16535177
Steve <sntakirutimana72@gmail.com>
2025-05-04 11:43:40 +0200

    [Service] Added core text processing operations (READ & WRITE)

---
+++ src/main/java/com/automating_text_processing/service/TextProcessingCoreService.java
@@ -0,0 +1,28 @@
+package com.automating_text_processing.service;
+
+import com.automating_text_processing.rule.ReadRule;
+import com.automating_text_processing.rule.WriteRule;
+import com.automating_text_processing.strategy.FileReaderContext;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+
+public abstract class TextProcessingCoreService {
+
+  public void read(ReadRule rule) throws IOException {
+    // Get the right reading strategy based on file size, and then read & consume data
+    FileReaderContext.getStrategy(rule.getPath()).read(rule);
+  }
+
+  public void write(WriteRule rule) throws IOException {
+    try (BufferedWriter writer = Files.newBufferedWriter(rule.getPath(), StandardCharsets.UTF_8, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
+      while (rule.getReadIterator().hasNext()) {
+        writer.write(rule.getReadIterator().next());
+        writer.newLine();
+      }
+    }
+  }
+}
